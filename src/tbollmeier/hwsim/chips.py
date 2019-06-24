dba3b6a17dc2ee1c26032da1ef1b6e63140d9153
Thomas Bollmeier <developer@thomas-bollmeier.de>
2019-06-24 21:56:48 +0200

    Chip builder implemented

--- src/tbollmeier/hwsim/chips.py
+++ src/tbollmeier/hwsim/chips.py
@@ -1,36 +1,44 @@
 from tbollmeier.hwsim.builtin import Nand
 from tbollmeier.hwsim.bit_value import BitValue
-from tbollmeier.hwsim.composed_chip import ComposedChip
+from tbollmeier.hwsim.composed_chip import ChipBuilder
 
 
-def make_not():
+Not = ChipBuilder()\
+    .add_input("in")\
+    .add_output("out")\
+    .add_internal_part("nand", Nand)\
+    .add_connection("in", "nand.a")\
+    .set_input_bit("nand.b", BitValue.ONE)\
+    .add_connection("nand.out", "out")
 
-    return ComposedChip()\
-        .add_input("in")\
-        .add_output("out")\
-        .add_internal_part("nand", Nand())\
-        .connect("in", "nand.a")\
-        .set_input_bit("nand.b", BitValue.ONE)\
-        .connect("nand.out", "out")
+Or = ChipBuilder()\
+    .add_input("a")\
+    .add_input("b")\
+    .add_output("out")\
+    .add_internal_part("nota", Not)\
+    .add_internal_part("notb", Not)\
+    .add_internal_part("nand", Nand)\
+    .add_connection("a", "nota.in")\
+    .add_connection("nota.out", "nand.a")\
+    .add_connection("b", "notb.in")\
+    .add_connection("notb.out", "nand.b")\
+    .add_connection("nand.out", "out")
 
-
-def make_or():
-
-    chip = ComposedChip()
-    chip.add_input("a").add_input("b").add_output("out")
-    chip.add_internal_part("nota", make_not())
-    chip.add_internal_part("notb", make_not())
-    chip.add_internal_part("nand", Nand())
-    chip.connect("a", "nota.in").connect("nota.out", "nand.a")
-    chip.connect("b", "notb.in").connect("notb.out", "nand.b")
-    chip.connect("nand.out", "out")
-
-    return chip
+And = ChipBuilder()\
+    .add_input("a")\
+    .add_input("b")\
+    .add_output("out")\
+    .add_internal_part("nand", Nand)\
+    .add_internal_part("not", Not)\
+    .add_connection("a", "nand.a")\
+    .add_connection("b", "nand.b")\
+    .add_connection("nand.out", "not.in")\
+    .add_connection("not.out", "out")
 
 
 if __name__ == "__main__":
 
-    chip = make_or()
+    chip = And()
 
     for a in [0, 1]:
         chip.set_input_bit("a", a)
--- src/tbollmeier/hwsim/composed_chip.py
+++ src/tbollmeier/hwsim/composed_chip.py
@@ -9,17 +9,61 @@ class ComponentType(object):
     INTERNAL = 3
 
 
-class ComposedChip(Chip):
+class ChipBuilder(object):
 
     def __init__(self):
-        self._parts = {}
+        self._inputs = []
+        self._outputs = []
+        self._parts = []
+        self._connections = []
+        self._input_bits = []
 
     def add_input(self, name, num_pins=1):
-        self._parts[name] = (Input(name, num_pins), ComponentType.INPUT)
+        self._inputs.append((name, num_pins))
         return self
 
     def add_output(self, name, num_pins=1):
-        self._parts[name] = (Connector(name, num_pins), ComponentType.OUTPUT)
+        self._outputs.append((name, num_pins))
+        return self
+
+    def add_internal_part(self, name, chip_builder):
+        self._parts.append((name, chip_builder))
+        return self
+
+    def add_connection(self, name_src, name_tgt, pin_range_src=None, pin_range_tgt=None):
+        self._connections.append((name_src, name_tgt, pin_range_src, pin_range_tgt))
+        return self
+
+    def set_input_bit(self, full_name, value, pin=0):
+        self._input_bits.append((full_name, value, pin))
+        return self
+
+    def __call__(self):
+        chip = ComposedChip()
+        for name, num_pins in self._inputs:
+            chip.add_input(name, num_pins)
+        for name, num_pins in self._outputs:
+            chip.add_output(name, num_pins)
+        for name, chip_builder in self._parts:
+            chip.add_internal_part(name, chip_builder())
+        for source, target, pin_range_src, pin_range_tgt in self._connections:
+            chip.connect(source, target, pin_range_src, pin_range_tgt)
+        for full_name, value, pin in self._input_bits:
+            chip.set_input_bit(full_name, value, pin)
+        return chip
+
+
+class ComposedChip(Chip):
+
+    def __init__(self):
+        self._pins = {}
+
+    def add_input(self, name, num_pins=1):
+        self._pins[name] = (Input(name, num_pins), ComponentType.INPUT)
+        return self
+
+    def add_output(self, name, num_pins=1):
+        self._pins[name] = (Connector(name, num_pins), ComponentType.OUTPUT)
         return self
 
     def add_internal_part(self, name, chip):
@@ -27,27 +71,27 @@ class ComposedChip(Chip):
         output_names = chip.get_output_names()
         for input_name in input_names:
             input_ = chip.get_input(input_name)
-            self._parts[name + "." + input_name] = (input_, ComponentType.INTERNAL)
+            self._pins[name + "." + input_name] = (input_, ComponentType.INTERNAL)
         for output_name in output_names:
             output = chip.get_output(output_name)
-            self._parts[name + "." + output_name] = (output, ComponentType.INTERNAL)
+            self._pins[name + "." + output_name] = (output, ComponentType.INTERNAL)
         return self
 
     def connect(self, name_src, name_tgt, pin_range_src=None, pin_range_tgt=None):
-        target, _ = self._parts[name_tgt]
-        source, _ = self._parts[name_src]
+        target, _ = self._pins[name_tgt]
+        source, _ = self._pins[name_src]
         target.connect(source, pin_range_src, pin_range_tgt)
         return self
 
     def get_input_names(self):
-        return [input_.name for input_, type_ in self._parts.values() if type_ == ComponentType.INPUT]
+        return [input_.name for input_, type_ in self._pins.values() if type_ == ComponentType.INPUT]
 
     def get_output_names(self):
-        return [output_.name for output_, type_ in self._parts.values() if type_ == ComponentType.OUTPUT]
+        return [output_.name for output_, type_ in self._pins.values() if type_ == ComponentType.OUTPUT]
 
     def get_input(self, name):
         try:
-            input_, type_ = self._parts[name]
+            input_, type_ = self._pins[name]
             if type_ == ComponentType.INPUT:
                 return input_
             else:
@@ -56,13 +100,13 @@ class ComposedChip(Chip):
             return None
 
     def set_input_bit(self, full_name, value, pin=0):
-        input_, _ = self._parts[full_name]
+        input_, _ = self._pins[full_name]
         input_.set_bit(value, pin)
         return self
 
     def get_output(self, name):
         try:
-            output_, type_ = self._parts[name]
+            output_, type_ = self._pins[name]
             if type_ == ComponentType.OUTPUT:
                 return output_
             else:
